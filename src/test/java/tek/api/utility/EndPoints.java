9526f29a6d8aa4dd63ab22939e5332b1a8548840
Israr <israr.akh03@gmail.com>
2023-05-28 11:33:35 -0400

    Added Get Account Test

--- src/test/java/tek/api/utility/EndPoints.java
+++ src/test/java/tek/api/utility/EndPoints.java
@@ -5,7 +5,8 @@ public enum EndPoints {
 	TOKEN_GENERATION("/api/token"), 
 	TOKEN_VERIFY("/api/token/verify"),
 	Get_All_Accounts("/api/accounts/get-all-accounts"),
-	Get_Account("/api/accounts/get-account");
+	Get_Account("/api/accounts/get-account"),
+	Add_Primary_Account("/api/accounts/add-primary-account");
 	
 
 	private String value;
