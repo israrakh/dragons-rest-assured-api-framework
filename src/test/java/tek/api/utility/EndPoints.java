4bbd665af3c207ae227b01cdf01471ea21a10f07
Israr <israr.akh03@gmail.com>
2023-06-03 11:32:21 -0400

    EndPoints class has modified

--- src/test/java/tek/api/utility/EndPoints.java
+++ src/test/java/tek/api/utility/EndPoints.java
@@ -8,9 +8,7 @@ public enum EndPoints {
 	GET_ALL_ACCOUNTS("/api/accounts/get-all-accounts"),
 	GET_ACCOUNT("/api/accounts/get-account"),
 	ADD_PRIMARY_ACCOUNT("/api/accounts/add-primary-account"),
-	GET_ALL_PLAN_CODE("/api/plans/get-all-plan-code"),
-	USER_PROFILE("/api/user/profile");
-	
+	GET_ALL_PLAN_CODE("/api/plans/get-all-plan-code");
 
 	private String value;
 
