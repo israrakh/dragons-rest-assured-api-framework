347c5978220c2153aec366ad825609392c7d3c5d
Israr <israr.akh03@gmail.com>
2023-06-03 18:20:48 -0400

    project got update

--- src/test/java/tek/api/sqa/base/APITestConfig.java
+++ src/test/java/tek/api/sqa/base/APITestConfig.java
@@ -6,6 +6,8 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import tek.api.model.PrimaryAccount;
+import tek.api.utility.DataGenerator;
 import tek.api.utility.EndPoints;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +17,9 @@ import org.testng.annotations.Listeners;
 
 @Listeners({ ExtentITestListenerAdapter.class })
 public class APITestConfig extends BaseConfig {
-
+	
+	private DataGenerator dataGenerator = new DataGenerator();
+	
 	@BeforeMethod
 	public void setupApiTest() {
 		System.out.println("Setting up Test");
@@ -31,5 +35,20 @@ public class APITestConfig extends BaseConfig {
 		Response response = request.when().post(EndPoints.TOKEN_GENERATION.getValue());
 		String token = response.jsonPath().get("token");
 		return token;
+	}
+	
+	public PrimaryAccount createAccountData() {
+		PrimaryAccount account = new PrimaryAccount();
+		String firstName = dataGenerator.getFirstName();
+		String lastName = dataGenerator.getLastName();
+		account.setEmail(dataGenerator.getEmail(firstName, lastName, "@tekschool.us"));
+		account.setFirstName(firstName);
+		account.setLastName(lastName);
+		account.setTitle("Mr.");
+		account.setMaritalStatus("SINGLE");
+		account.setEmploymentStatus(dataGenerator.getJobTitle());
+		account.setDateOfBirth(dataGenerator.getDateOfBirth());
+		return account;
+		
 	}
 }
---
+++ src/test/java/tek/api/sqa/tests/CreateAccountWithDBVerification.java
@@ -0,0 +1,14 @@
+package tek.api.sqa.tests;
+
+import org.testng.annotations.Test;
+
+import tek.api.sqa.base.DatabaseConfig;
+
+public class CreateAccountWithDBVerification extends DatabaseConfig {
+	
+	@Test
+	public void createAccountTestDb() {
+		
+		
+	}
+}
